31129e0d8e110b7b3a374c7f18535a63a827da50
Deepan dpn <developerdpn06@gmail.com>
2021-01-31 21:12:39 -0800

    Update program_test_1.c

--- program_test_1.c
+++ program_test_1.c
@@ -6,17 +6,17 @@ void main()
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=(2*n)-i;j++){
-            if (j<=n-i)
+            while(j<=n-i)
             {
                 printf("");
             }
-            else if (i%2==1)
+            while (i%2==1)
             {
                 k++;
                 printf("%d ",k);
 
             }
-            else
+            while
             {
                 printf("%d ",k);
                 k--;
