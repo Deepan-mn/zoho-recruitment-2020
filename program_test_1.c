4252d18c4858117dd11aad091bca98efeedef842
Deepan dpn <developerdpn06@gmail.com>
2021-01-31 20:35:21 -0800

    Update program_test_1.c

--- program_test_1.c
+++ program_test_1.c
@@ -5,7 +5,7 @@ void main()
     Scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=(2*n)-1;j++){
+        for(j=1;j<=(2*n)-i;j++){
             if (j<=n-i)
             {
                 printf("");
